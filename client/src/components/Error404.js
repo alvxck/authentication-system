72256d373076e6d2f788246852f736ad9002dfeb
Alex <alexander_sck@live.ca>
2022-05-21 00:16:36 -0400

    feat: create error404 UI

--- client/src/components/Error404.js
+++ client/src/components/Error404.js
@@ -1,11 +1,18 @@
 import React from 'react'
-import './Error404.css'
+import { Link } from 'react-router-dom'
+import styles from './Error404.module.css'
 
 
 function Error() {
     return (
-        <div>
-            <h1>404: This page does not exist.</h1> 
+        <div className={styles.overlay}>
+            <div>
+                <h1 className={styles.header}>Error404.</h1> 
+                <p className={styles.text}>Page not found</p>
+                <Link className={styles.textLink} to='/login'>Login ↗</Link>
+                <br/>
+                <Link className={styles.textLink} to='/register'>Register ↗</Link>
+            </div>
         </div>
     )
 
