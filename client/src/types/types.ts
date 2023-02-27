dc4312be5437e4b428d79df234f3faaf709a6988
Alex Carvalho <alexander_sck@live.ca>
2023-02-27 18:43:48 -0500

    feat: create login and registration types

---
+++ client/src/types/types.ts
@@ -0,0 +1,13 @@
+
+export type registrationCredentials = {
+    event: React.FormEvent, 
+    name: string, 
+    email: string, 
+    password: string,
+}
+
+export type loginCredentials = {
+    event: React.FormEvent, 
+    email: string, 
+    password: string,
+}
