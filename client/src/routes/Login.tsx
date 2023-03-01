ee4271c08b4bd08fcec3b81761e59251fe74444f
Alex Carvalho <alexander_sck@live.ca>
2023-03-01 13:15:45 -0500

    feat: update login and registration page UI

--- client/src/routes/Login.tsx
+++ client/src/routes/Login.tsx
@@ -27,38 +27,44 @@ export const Login = () => {
     }
 
     return (
-        <div className='main'>
-            <form onSubmit={login}>
-                <h1>Login</h1>
-                <label>Email</label>
-                <input
-                    placeholder='Email'
-                    value={email}
-                    onChange={(x) => setEmail(x.target.value)}
-                    type='email'
-                />
-                <label>Password</label>
-                <input
-                    placeholder='Password'
-                    value={password}
-                    onChange={(x) => setPassword(x.target.value)}
-                    type={showPassword ? 'text' : 'password'}
-                />
-                <div>
+        <div className='m-0 p-0 flex items-center justify-center w-screen h-screen bg-neutral-900'>
+            <form
+                className='flex flex-col justify-between w-72'  
+                onSubmit={login}>
+                <h1 className='text-white text-center text-4xl'>Login</h1>
+                <div className='p-3 mt-5 flex flex-col justify-between border rounded-lg border-neutral-700 bg-neutral-800'>
+                    <label className='text-white mt-2'>Email</label>
                     <input
-                        type='checkbox'
-                        checked={showPassword}
-                        onChange={togglePassword}
+                        className='px-1 py-0.5 text-white border rounded border-neutral-700 bg-neutral-900 focus:border-blue-500 focus:outline-none'
+                        value={email}
+                        onChange={(x) => setEmail(x.target.value)}
+                        type='email'
                     />
-                    <label> Show Password</label>
+                    <label className='text-white mt-2'>Password</label>
+                    <input
+                        className='px-1 py-0.5 text-white border rounded border-neutral-700 bg-neutral-900 focus:border-blue-500 focus:outline-none'
+                        value={password}
+                        onChange={(x) => setPassword(x.target.value)}
+                        type={showPassword ? 'text' : 'password'}
+                    />
+                    <div>
+                        <input
+                            type='checkbox'
+                            checked={showPassword}
+                            onChange={togglePassword}
+                        />
+                        <label className='text-white text-sm'> Show Password</label>
+                    </div>
+                    <label>{error}</label>
+                    <input
+                        className='text-white mt-2 py-0.5 bg-green-500 rounded cursor-pointer' 
+                        type='submit'
+                        value='Login'
+                    />
                 </div>
-                <label>{error}</label>
-                <input 
-                    type='submit'
-                    value='Register'
-                />
-                <label>Don't have an account?
-                    <Link to='/register'>Register</Link>
+                
+                <label className='text-white text-center mt-3'>Don't have an account?
+                    <Link className='text-blue-500' to='/register'> Register↗︎</Link>
                 </label>
             </form>
         </div>
--- client/src/routes/Register.tsx
+++ client/src/routes/Register.tsx
@@ -28,45 +28,51 @@ export const Register = () => {
     }
 
     return (
-        <div className='main'>
-            <form onSubmit={register}>
-                <h1>Register</h1>
-                <label>Name</label>
-                <input
-                    placeholder='Name'
-                    value={name}
-                    onChange={(x) => setName(x.target.value)}
-                    type='text'
-                />
-                <label>Email</label>
-                <input
-                    placeholder='Email'
-                    value={email}
-                    onChange={(x) => setEmail(x.target.value)}
-                    type='email'
-                />
-                <label>Password</label>
-                <input
-                    placeholder='password'
-                    value={password}
-                    onChange={(x) => setPassword(x.target.value)}
-                    type={showPassword ? 'text' : 'password'}
-                />
-                <div>
+        <div className='m-0 p-0 flex items-center justify-center w-screen h-screen bg-neutral-900'>
+            <form
+                className='flex flex-col justify-between w-72'  
+                onSubmit={register}>
+                <h1 className='text-white text-center text-4xl'>Register</h1>
+                <div className='p-3 mt-5 flex flex-col justify-between border rounded-lg border-neutral-700 bg-neutral-800'>
+                    <label className='text-white'>Name</label>
                     <input
-                        type='checkbox'
-                        checked={showPassword}
-                        onChange={togglePassword}
+                        className='px-1 py-0.5 text-white border rounded border-neutral-700 bg-neutral-900 focus:border-blue-500 focus:outline-none'
+                        value={name}
+                        onChange={(x) => setName(x.target.value)}
+                        type='text'
                     />
-                    <label> Show Password</label>
+                    <label className='text-white mt-2'>Email</label>
+                    <input
+                        className='px-1 py-0.5 text-white border rounded border-neutral-700 bg-neutral-900 focus:border-blue-500 focus:outline-none'
+                        value={email}
+                        onChange={(x) => setEmail(x.target.value)}
+                        type='email'
+                    />
+                    <label className='text-white mt-2'>Password</label>
+                    <input
+                        className='px-1 py-0.5 text-white border rounded border-neutral-700 bg-neutral-900 focus:border-blue-500 focus:outline-none'
+                        value={password}
+                        onChange={(x) => setPassword(x.target.value)}
+                        type={showPassword ? 'text' : 'password'}
+                    />
+                    <div>
+                        <input
+                            type='checkbox'
+                            checked={showPassword}
+                            onChange={togglePassword}
+                        />
+                        <label className='text-white text-sm'> Show Password</label>
+                    </div>
+                    <label>{error}</label>
+                    <input
+                        className='text-white mt-2 py-0.5 bg-green-500 rounded cursor-pointer' 
+                        type='submit'
+                        value='Register'
+                    />
                 </div>
-                <label>{error}</label>
-                <input 
-                    type='submit'
-                    value='Register'
-                />
-                <label>Already have an account?
-                    <Link to='/login'>Login</Link>
+                
+                <label className='text-white text-center mt-3'>Already have an account?
+                    <Link className='text-blue-500' to='/login'> Login↗</Link>
                 </label>
             </form>
         </div>
